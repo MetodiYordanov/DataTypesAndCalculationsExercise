5e065423f95cf1f585bfb7e9d9d4bd8f1e501ce7
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 16:18:15 +0200

    Add description for "Deposit Calculator" exercise.

--- DepositCalculator/Program.cs
+++ DepositCalculator/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            // Program gets input from the console for deposited amount, term of deposit and annual interest rate.
+            // Then it calculates accumulated interest and interest for one month.
+            // In the end, program calculates total amount and prints it on the console.
+
             double depositedAmount = double.Parse(Console.ReadLine());
             int termOfDeposit = int.Parse(Console.ReadLine());
             double annualInterestRate = double.Parse(Console.ReadLine());
