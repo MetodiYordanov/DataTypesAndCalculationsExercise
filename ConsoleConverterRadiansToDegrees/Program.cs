a06c396f64ec745ba45d8747151cfd54e0f8be67
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 16:07:35 +0200

    Add description of the code for "Console converter: from radians to degrees" exercise.

--- ConsoleConverterRadiansToDegrees/Program.cs
+++ ConsoleConverterRadiansToDegrees/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            // The program reads an angle in radians and converts it to degrees.
+            // After the convert is made, result is printed on the console.
+            // Example - Input: 6.2832; Output: 360.0008418365988
+
             double numberInRadians = double.Parse(Console.ReadLine());
             double numberInDegrees = numberInRadians * 180 / Math.PI;
             Console.WriteLine(numberInDegrees);
