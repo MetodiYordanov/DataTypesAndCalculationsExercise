dc931b0dff274db68f5ad8fc63ccda1da858649e
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 16:29:02 +0200

    Add description for "Mandatory Literature" exercise.

--- MandatoryLiterature/Program.cs
+++ MandatoryLiterature/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+            // The program calculates how many hours user should spent on reading a book for one day, so he reads it on time.
+            // User enters from the console number of book's pages, pages that he reads for 1 hour and the number of days he needs to finish the book.
+            // Example - Input: book's pages = 212, pages that user reads for 1 hour = 20, number of days to finish book = 2
+            // Output: 5 hours per day
+
             int numberOfPages = int.Parse(Console.ReadLine());
             int pagesPerHour = int.Parse(Console.ReadLine());
             int numberOfDaysToRead = int.Parse(Console.ReadLine());
