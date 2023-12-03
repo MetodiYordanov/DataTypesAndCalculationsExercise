535f0b771cbaf8f1e92c08bdda801b86f8c54056
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 16:38:08 +0200

    Add description for "Teaching Materials" exercise.

--- TeachingMaterials/Program.cs
+++ TeachingMaterials/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            // The program calculates how much money user will need to collect to pay the final bill.
+            // For input, user shoud write number of packages of pens, number of packets of markers,
+            // liters of board cleaner and discount percentage as user is regular client.
+            // Example - Input: number of packages of pens = 2, number of packets of markers = 3,
+            // liters of board ceaner = 4, discount percentage = 25%
+            // Output: 28.5
+
             const double pricePackageOfPens = 5.8;
             const double pricePackageOfMarkers = 7.2;
             const double priceBoardCleanerPerLiter = 1.2;
