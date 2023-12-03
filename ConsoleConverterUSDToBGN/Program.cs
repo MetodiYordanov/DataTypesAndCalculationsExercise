de34bfa0fe2560b097658978a68d0ddbdd4067e0
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 16:00:47 +0200

    Add comment describing "Console Converter: USD to BGN" exercise.

--- ConsoleConverterUSDToBGN/Program.cs
+++ ConsoleConverterUSDToBGN/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            // The code converts US dollars to Bulgarian leva. 
+            // The fixed rate between dollar and lev is 1 USD = 1.79549 BGN.
+            // Example - Input: 22, Output: 39.50078
+
             const double multiplier = 1.79549;
             double sumInDollars = double.Parse(Console.ReadLine());
             double sumInLeva = sumInDollars * multiplier;
