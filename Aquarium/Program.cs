2c2aec6a0f1819747d3fb5bfcc7553dc5290c50e
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 17:08:00 +0200

    Add description for "Aquarium" exercise

--- Aquarium/Program.cs
+++ Aquarium/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            // The program calculates liters of water needed tofill aquarium.
+            // For input user enters length, width and height of the aquarium in cm
+            // and also percentage of the aquarium that will be occupied by sand, plants, heater and pump.
+            // User also knows that one liter equals one cubic decimeter (1l = 1dm3).
+            // Example - Input: length = 85, width = 75, height = 47, percentage = 17
+            // Output: 248.69
+
             int length = int.Parse(Console.ReadLine());
             int width = int.Parse(Console.ReadLine());
             int height = int.Parse(Console.ReadLine());
