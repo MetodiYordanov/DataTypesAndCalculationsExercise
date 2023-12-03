5a7f84c139d4f1afac0f54ff4d6eb744bf332f13
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 16:21:54 +0200

    Add example in the description for "Deposit Calculator" exercise.

--- DepositCalculator/Program.cs
+++ DepositCalculator/Program.cs
@@ -7,7 +7,8 @@
             // Program gets input from the console for deposited amount, term of deposit and annual interest rate.
             // Then it calculates accumulated interest and interest for one month.
             // In the end, program calculates total amount and prints it on the console.
-
+            // Example - Input: deposited amount = 200, term of deposit = 3 months, annual interest rate = 5.7%
+            // Output: 202.85
             double depositedAmount = double.Parse(Console.ReadLine());
             int termOfDeposit = int.Parse(Console.ReadLine());
             double annualInterestRate = double.Parse(Console.ReadLine());
