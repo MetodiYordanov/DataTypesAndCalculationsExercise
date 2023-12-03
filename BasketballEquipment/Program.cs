ec16670b658e9fcff5e7a153874069a7de78252d
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-03 17:00:31 +0200

    Add description for "Basketball equipment" exercise.

--- BasketballEquipment/Program.cs
+++ BasketballEquipment/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            // The program calculates the expenses of user's basketball equipment and training fee for 1 year.
+            // User also knows that:
+            // basketball sneakers are 40% less than the training fee for one year
+            // basketball team is 20% cheaper than the price of sneakers
+            // basketball is 25% of the price of the basketball team
+            // basketball accessories price is 20% of the price of the basketball
+            // Example - Input: annual basketball training fee = 365
+            // Output: 811.76
+
             int annualTrainingFee = int.Parse(Console.ReadLine());
 
             double sneakers = annualTrainingFee - (annualTrainingFee * 0.4);
